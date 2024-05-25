dfb50b5cb91d55b5adf881193e87d7526fb8826c
muhammadstand <muhammadstand89@gmail.com>
2024-05-25 10:29:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let tema=document.querySelector(".tema");
+tema.onclick=function(){
+    if (confirm("вы нажли на кнопку")){
+        document.body.classList.toggle("dark")
+    }
+}
+let ttt=document.querySelector(".ttt");
+tema.onclick=function(){
+    if (confirm("дратути")){
+        document.body.classList.toggle("dark")
+    }
+}
